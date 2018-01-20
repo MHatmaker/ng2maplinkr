c60d20c45a4dc47d6769dced55b81e3b0acde63f
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2018-01-19 17:11:07 -0700

    updates configparams file missed in last commit

--- app/services/configparams.service.ts
+++ app/services/configparams.service.ts
@@ -19,15 +19,5 @@ export class ConfigParams implements IConfigParams{
          public webmapId : string,
          public mlposition : IPosition) {
 
-        // this.mapId = mapId;
-        // this.mapType = mapType;
-        // this.webmapId = webmapId;
-        // if (mlposition) {
-        //     this.mlposition.lon = mlposition.lon;
-        //     this.mlposition.lat = mlposition.lat;
-        //     this.mlposition.zoom = mlposition.zoom;
-        // } else {
-        //     this.mlposition.lon = this.mlposition.lat = this.mlposition.zoom = -1;``
-        // }
     }
 };
